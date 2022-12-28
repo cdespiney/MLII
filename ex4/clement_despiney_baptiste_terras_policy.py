cf06f7131deaab9902edc365505f8112d3b4ea78
Clément Despiney <c.despiney@gmail.com>
2022-12-28 17:43:36 +0100

    feat: ex4 done

--- ex4/clement_despiney_baptiste_terras_policy.py
+++ ex4/clement_despiney_baptiste_terras_policy.py
@@ -9,30 +9,50 @@ def new_policy(agent: Agent) -> str:
 
     gamma = 0.5
 
+    """
+    We first check if some 'static' variables exist, else we create them
+    """
     if not hasattr(new_policy, "value_function"):
         new_policy.value_function = [0 for x in agent.known_rewards]
     if not hasattr(new_policy, "timer"):
         new_policy.timer = 0
 
+    """
+    We increment or reset the timer
+    """
     new_policy.timer += 1
 
     if new_policy.timer % 10 == 0:
         new_policy.timer = 0
 
+    """
+    We compute the value function
+    """
     for i in range(0, len(new_policy.value_function)):
         new_policy.value_function[i] = agent.known_rewards[i] + max(
             gamma * new_policy.value_function[i-1] if i != 0 else 0,
             gamma * new_policy.value_function[i + 1] if i < 7 else 0
         )
 
+    """
+    If we are in exploration mode, we perform a random walk
+    """
     if new_policy.timer < 5:
         actions = ["left", "right"]
         return actions[random.randint(0, 1)]
     
+    """
+    Else, we are in the exploitation mode, we follow the increases of the value function
+    """
     if (agent.position != 7 and new_policy.value_function[agent.position] < new_policy.value_function[agent.position + 1]) and (agent.position != 0 and new_policy.value_function[agent.position] <  new_policy.value_function[agent.position -1]):
         return "left" if new_policy.value_function[agent.position - 1] >= new_policy.value_function[agent.position + 1] else "right"
     if agent.position != 0 and new_policy.value_function[agent.position] <  new_policy.value_function[agent.position - 1]:
         return "left"
     if agent.position != 7 and new_policy.value_function[agent.position] <  new_policy.value_function[agent.position + 1]:
         return "right"
+
+    """
+    If we avoided all the cases above, we are at the (probably local) maximum of the value function.
+    We do not need to move anymore, simply collect the rewards.
+    """
     return "none"
